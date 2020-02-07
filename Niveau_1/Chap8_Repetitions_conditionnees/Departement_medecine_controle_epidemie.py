1d74a66711300362367c9881cc74fc7636d9d7aa
Kevin Cortellari <kevin.cortellari@gmail.com>
2020-02-07 14:55:25 +0100

    update comments

--- Niveau_1/Chap8_Repetitions_conditionnees/Departement_medecine_controle_epidemie.py
+++ Niveau_1/Chap8_Repetitions_conditionnees/Departement_medecine_controle_epidemie.py
@@ -1,7 +1,10 @@
+# initialisation des variables
 popuVille = int(input())
 nbMalades = 1
 nbJours = 1
+# boucle sur condition
 while nbMalades < popuVille:
    nbMalades = nbMalades + (2 * nbMalades)
    nbJours = nbJours +1
+# rendu resultat
 print(nbJours)
